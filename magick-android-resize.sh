d8ea5098041cbbd5e43bc82c4f8dde89dd64d4d1
Felipe Castilhos <felipecastilhos@gmail.com>
2014-09-30 07:46:34 -0300

    remove debug

--- magick-android-resize.sh
+++ magick-android-resize.sh
@@ -2,7 +2,7 @@
 
 DIR=$1
 
-XXHDPI="$DIR/drawable-xxhpdi"
+XXHDPI="$DIR/drawable-xxhdpi"
 XHDPI="$DIR/drawable-xhdpi"
 HDPI="$DIR/drawable-hdpi"
 MDPI="$DIR/drawable-mdpi"
@@ -22,18 +22,15 @@ IMAGE_WIDTH=$(identify $i | awk ' {print $3} ' | awk ' BEGIN{FS="x"} {print $2}'
 #CREATE MDPI
 MDPI_SIZE=$(echo "${IMAGE_WIDTH} / 3" | bc -l) 
 convert $i -resize $MDPI_SIZE $MDPI/$FILENAME; 
-echo $MDPI_SIZE
 
 
 #CREATE HDPI
 HDPI_SIZE=$(echo "${MDPI_SIZE} * 1.5" | bc -l)
 convert $i -resize $HDPI_SIZE $HDPI/$FILENAME; 
-echo $HDPI_SIZE
 
 #CREATE XHDPI
 XHDPI_SIZE=$(echo "${MDPI_SIZE} * 2" | bc -l)
 convert $i -resize $XHDPI_SIZE $XHDPI/$FILENAME; 
-echo $XHDPI_SIZE
 
 #CREATE XXHDPI
 mv $i $XXHDPI
