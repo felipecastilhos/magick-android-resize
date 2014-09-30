fcc23783d13cb819dba4887c0f207c1a53cc00c4
Felipe Castilhos <felipecastilhos@gmail.com>
2014-09-30 09:31:15 -0300

    Added option to rescale XXHDPI first

--- magick-android-resize.sh
+++ magick-android-resize.sh
@@ -2,13 +2,11 @@
 
 DIR=$1
 
-
-#BASE_RESIZE is set as 3, because android guidelines set MDPI as XXHDPI/3
-if [  -z $2 ]; then
-	BASE_RESIZE=3
-else  
-	BASE_RESIZE=$2
+if [ ! -z $2 ]; then
+	XX_RESIZE=$2
 fi
+
+echo $XX_RESIZE
 
 XXHDPI="$DIR/drawable-xxhdpi"
 XHDPI="$DIR/drawable-xhdpi"
@@ -23,11 +21,16 @@ $(mkdir $MDPI)
 
 for i in $(ls $DIR/*.png); do
 
+if [ ! -z $XX_RESIZE ]; then
+	echo $XX_RESIZE
+	convert $i -resize $XX_RESIZE $i
+fi
+
 FILENAME=$(echo $i | tr / , | awk 'BEGIN {FS=","} {print $NF}')
 IMAGE_WIDTH=$(identify $i | awk ' {print $3} ' | awk ' BEGIN{FS="x"} {print $2}')
 
 #CREATE MDPI
-MDPI_SIZE=$(echo "${IMAGE_WIDTH} / ${BASE_RESIZE} " | bc -l) 
+MDPI_SIZE=$(echo "${IMAGE_WIDTH} / 3 " | bc -l) 
 convert $i -resize $MDPI_SIZE $MDPI/$FILENAME; 
 
 
