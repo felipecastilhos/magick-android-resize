1b8a3afb02471eb0be36c9dac42eb57c82f4654c
Felipe Castilhos <felipecastilhos@gmail.com>
2014-11-19 13:22:33 -0200

    generate XXXHDPI and LDPI

--- magick-android-resize.sh
+++ magick-android-resize.sh
@@ -8,16 +8,21 @@ fi
 
 echo $XX_RESIZE
 
+XXXHDPI="$DIR/drawable-xxxhdpi"
 XXHDPI="$DIR/drawable-xxhdpi"
 XHDPI="$DIR/drawable-xhdpi"
 HDPI="$DIR/drawable-hdpi"
 MDPI="$DIR/drawable-mdpi"
+LDPI="$DIR/drawable-ldpi"
 
 #CREATE DIR
+$(mkdir $XXXHDPI)
 $(mkdir $XXHDPI)
 $(mkdir $XHDPI)
 $(mkdir $HDPI)
 $(mkdir $MDPI)
+$(mkdir $LDPI)
+
 
 for i in $(ls $DIR/*.png); do
 
@@ -35,6 +40,11 @@ echo $MDPI_SIZE
 convert $i -resize x$MDPI_SIZE $MDPI/$FILENAME; 
 
 
+#CREATE LDPI
+LDPI_SIZE=$(echo "${MDPI_SIZE} * 0.7" | bc -l) 
+echo $LDPI_SIZE
+convert $i -resize x$LDPI_SIZE $LDPI/$FILENAME; 
+
 #CREATE HDPI
 HDPI_SIZE=$(echo "${MDPI_SIZE} * 1.5" | bc -l)
 convert $i -resize x$HDPI_SIZE $HDPI/$FILENAME; 
@@ -43,13 +53,11 @@ convert $i -resize x$HDPI_SIZE $HDPI/$FILENAME;
 XHDPI_SIZE=$(echo "${MDPI_SIZE} * 2" | bc -l)
 convert $i -resize x$XHDPI_SIZE $XHDPI/$FILENAME; 
 
+#CREATE XXXHDPI
+XXXHDPI_SIZE=$(echo "${MDPI_SIZE} * 4" | bc -l)
+convert $i -resize x$XXXHDPI_SIZE $XXXHDPI/$FILENAME; 
+
 #CREATE XXHDPI
 mv $i $XXHDPI
 
 done
-
-
-
-
-
-
